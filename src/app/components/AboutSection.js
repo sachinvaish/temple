3bc9982a1da53c4bd47ff1dd5a11c684e5cd0221
sachinvaish <vaish139@gmail.com>
2023-08-01 08:34:55 +0530

    hidden changes

--- src/app/components/AboutSection.js
+++ src/app/components/AboutSection.js
@@ -13,7 +13,7 @@ export default function AboutSection() {
       <div className="col-lg-8 col-12">
         <div className="about__contentpart ms-lg-4 ps-lg-5 border-start">
           <div className="section__header">
-            <h2>Bhagat Sh. Sanjeev Saini Ji</h2>
+            <h2>Temple Pujari Ji</h2>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolores id illo commodi, sint illum ullam ut maxime cumque doloremque, voluptates repellendus a corrupti animi quia incidunt fugiat nobis earum, ipsam nisi. Dolorum, quas. Eos optio eius at dicta delectus laboriosam molestiae necessitatibus doloremque aperiam asperiores omnis maiores accusantium iusto, unde hic a consequuntur fugiat, totam, soluta rem obcaecati sequi earum. Reiciendis rerum voluptatibus eum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea similique consequatur nemo velit explicabo molestias sequi neque eaque deserunt! Dolores, aperiam dolorum magni eius quibusdam dolorem beatae veritatis, tenetur nisi, enim quasi vero aliquam ipsam? Soluta placeat aliquam laboriosam eaque neque ipsam magnam eos sunt.
             </p>
